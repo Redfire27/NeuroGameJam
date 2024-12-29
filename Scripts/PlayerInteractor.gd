09563f751ccdecc9572752da63b60be39db24749
Ugurturkel <ugurumutturkel@hotmail.com>
2024-12-29 17:51:44 +0300

    balls

--- Scripts/PlayerInteractor.gd
+++ Scripts/PlayerInteractor.gd
@@ -6,31 +6,22 @@ var cached_closest: Interactable
 
 func _ready() -> void:
 	controller = player
-	
+
 func _physics_process(_delta: float) -> void:
 	var new_closest: Interactable = get_closest_interactable()
 	if new_closest != cached_closest:
 		if is_instance_valid(cached_closest):
 			unfocus(cached_closest)
-			
-			cached_closest = null
-
 		if new_closest:
 			focus(new_closest)
 
 		cached_closest = new_closest
-		
-		
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact"):
-		if is_instance_valid(cached_closest):  # Ensure validity before interacting
+		if is_instance_valid(cached_closest):
 			interact(cached_closest)
-		else:
-			print("Cannot interact: cached_closest is invalid.")
-			cached_closest = null  # Reset reference if invalid
 
 func _on_area_exited(area: Interactable) -> void:
 	if cached_closest == area:
-		if is_instance_valid(area):
-			unfocus(area)
-		cached_closest = null  # Reset cached_closest if the area exited
+		unfocus(area)
--- Scripts/blinds.gd
+++ Scripts/blinds.gd
@@ -3,19 +3,21 @@ extends Node3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 var is_open: bool = false
 
-
-func open() -> void:
-	if Input.is_action_just_pressed("interact"):
-		animation_player.play("Open")
-
-
-func close() -> void:
-	if Input.is_action_just_pressed("interact"):
-		animation_player.play("Close")
-
-
-
+# Called when the interactable item is interacted with
 func _on_interactable_interacted(interactor: Interactor) -> void:
-	if not is_open:
-		$Interactable.queue_free()
+	if is_open:
+		close()
+	else:
 		open()
+	
+	# Toggle the state
+	is_open = !is_open
+
+# Play the open animation
+func open() -> void:
+	animation_player.play("Open")
+
+# Play the close animation
+func close() -> void:
+	animation_player.play("Close")
+	
